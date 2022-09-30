b1cdd759232a00866ca5a8a0072bf198cac9561c
Sergey M <berserker992@gmail.com>
2022-09-30 16:28:38 +1100

    Sixth Project v.0.0.1

---
+++ 6.MaxValue/Program.cs
@@ -0,0 +1,14 @@
+﻿int v1 = 25;
+int v2 = 60;
+int v3 = 72;
+int v4 = 27;
+int v5 = 11;
+
+int max = v1;
+
+if(max < v2) max = v2;
+if(max < v3) max = v3;
+if(max < v4) max = v4;
+if(max < v5) max = v5;
+
+Console.WriteLine(string.Format("Maximum value is {0}", max));
