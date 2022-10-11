d4fe6bc90f67e0f355f3cc64d6faf9b53350103a
Sergey M <berserker992@gmail.com>
2022-10-12 03:50:05 +1100

    FourthHomework v0.2

---
+++ 16.FourthHomework - task 27/Program.cs
@@ -0,0 +1,36 @@
+﻿// Function to get correct values from user ...
+string GetNumeric(string msg)
+{
+
+    Console.WriteLine($"{msg}");
+    int positiveNumeric;
+    bool itsNumeric = int.TryParse(Console.ReadLine(), out positiveNumeric);
+
+    if (itsNumeric)
+
+        if (positiveNumeric >= 0)
+            return positiveNumeric.ToString();
+        else
+            Console.WriteLine($"Your numeric is less then zero ... ");
+
+    else
+        Console.WriteLine("It's not numeric!");
+
+    return string.Empty;
+
+}
+
+// Set variables ... 
+int result = 0;
+string A = string.Empty;
+
+// Get numeric as string ... 
+while (A.Length <= 0)
+    A = GetNumeric("Insert numeric to sum of digits:");
+
+// Get sum of digits to using a 'for' cycle ...
+for (int i = 0; i < A.Length; i++)
+    result = result + int.Parse(A[i].ToString());
+
+// Print result ...
+Console.WriteLine(result);
---
+++ 17.FourthHomework - task 29/Program.cs
@@ -0,0 +1,25 @@
+﻿// Show info about programm ...
+Console.WriteLine("Insert array of integers, set comma between numerics: ");
+
+// Set variable for save array ... 
+string[] A = new string[0];
+
+// Get data ... 
+while (A.Length <= 0)
+    A = Console.ReadLine().ToString().Split(',');
+
+
+Console.WriteLine($"{A.Length}");
+
+// Show array in the cycle ...
+for (int i = 0; i < A.Length; i++){
+
+    int Numeric;
+    bool itsNumeric = int.TryParse(A[i], out Numeric);
+    if(itsNumeric)
+        Console.WriteLine($"Element {i} = {A[i]}");
+    else
+        Console.WriteLine($"Element {i} = {A[i]}  // Not critical ERROR, your element isn't numeric!");
+
+}
+    
