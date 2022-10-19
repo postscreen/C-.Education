13fe6dec3448ea942970facdd368219387afebea
Sergey M <berserker992@gmail.com>
2022-10-19 22:36:20 +1100

    Sixth homework 0.1

---
+++ 22.SixthHomework - task 41/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+
+    Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+    0, 7, 8, -2, -2 -> 2
+    1, -7, 567, 89, 223-> 3
+
+*/
+
+(int, int) GetNumeric(int PositiveCounter = 0, int TotatCounter = 0)
+{
+
+    // Clear screen before start each times ...
+    Console.Clear();
+
+    // Show info and current state ...
+    Console.WriteLine($"Pleace, insert numeric or \"q\" to exit (Positive = {PositiveCounter}, Total = {TotatCounter}): ");
+    int usersNumeric = 0;
+
+    // Get string by the user and check, maybe user need to stop it ...
+    string usersInput  = Console.ReadLine();
+    if(usersInput == "q")
+        return (0,0);
+
+    // Increase counter if user enter correct numeric ...
+    bool itsNumeric = int.TryParse(usersInput, out usersNumeric);
+    if (itsNumeric)
+    {
+
+        TotatCounter++;
+        if (usersNumeric > 0)
+            PositiveCounter++;
+
+    }
+
+    // Continious go to the M count of numerics ...
+    return GetNumeric(PositiveCounter, TotatCounter);
+
+}
+
+GetNumeric();
---
+++ 23.SixthHomework - task 43/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+
+    Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+    заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+    b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+*/
+
+// To show conditions ...
+void showConditions() =>
+    Console.WriteLine("We are have [y = k1 * x + b1] and [y = k2 * x + b2] ... ");
+
+// To calculate inspections point ...
+(double,double) getInspectionPoint(double k1, double k2, double b1, double b2){
+
+    double x =  (b2 - b1) / (k1 - k2);
+    double y = (k1 * x) + b1;
+
+    return (x, y);
+
+}
+
+// Clear terminal ...
+Console.Clear();
+
+// Set data storage ...
+string[] dotNames = new string[4] { "k1", "k2", "b1", "b2" };
+double[] dotValues = new double[4] { 0, 0, 0, 0 };
+
+// Show conditions after start ...
+showConditions();
+
+// Collect points ...
+int i = 0;
+while( i < dotNames.Length ){
+
+    Console.WriteLine($"Insert {dotNames[i]} = ");
+    bool itsDouble = double.TryParse(Console.ReadLine(), out dotValues[i]);
+
+    if(itsDouble)
+        i++;
+    else
+        Console.WriteLine("Itsn't numeric ... Re enter pls ... ");
+
+}
+
+// Get inspections points ... 
+(double x, double y) = getInspectionPoint(dotValues[0], dotValues[1], dotValues[2], dotValues[3]);
+
+// Show and format total result of executions ...
+Console.Clear();
+showConditions();
+Console.WriteLine($"\r\n\tk1 = {dotValues[0]}, k2 = {dotValues[1]}, b1 = {dotValues[2]}, b2 = {dotValues[3]}");
+Console.WriteLine($"\tInspection point is Z( {x} ; {y} ) ... \r\n\r\n");
