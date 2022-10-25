945edd271b6775091d0e1f2e844a86584452f6a9
Sergey M <berserker992@gmail.com>
2022-10-26 02:20:06 +1100

    Eigth homework v0.1

--- 13.RegEx/Program.cs
+++ 13.RegEx/Program.cs
@@ -3,19 +3,42 @@ using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-string s = "Python is a fun programming language";
+//string s = "Python is a fun programming language";
+Random rnd = new Random();
+double randomValue = 0;
+
+
+
+/*
+start_time = DateTime.Now;
+foreach (int i in Enumerable.Range(0, 100000000))
+    randomValue = new Random().NextDouble() * 100;
+Console.WriteLine(string.Format("Total: {0} sec", (DateTime.Now - start_time).TotalSeconds));
+*/
+
 
 DateTime start_time = DateTime.Now;
 
-foreach(int i in Enumerable.Range(0,10000000))
-    s.IndexOf("fun");
 
-Console.WriteLine(string.Format("s.find: {0} sec", (DateTime.Now - start_time).TotalSeconds ));
+double max_range = 10.5;
+double min_range = 0;
+foreach (int i in Enumerable.Range(0, 100000000))
+    randomValue = rnd.NextDouble() * (max_range - min_range) + min_range;
+Console.WriteLine(string.Format("Total: {0} sec", (DateTime.Now - start_time).TotalSeconds));
 
 start_time = DateTime.Now;
-Regex r = new Regex(@"fun", RegexOptions.Compiled);
+foreach (int i in Enumerable.Range(0, 100000000))
+    randomValue = rnd.NextDouble();
+Console.WriteLine(string.Format("Total: {0} sec", (DateTime.Now - start_time).TotalSeconds));
 
-foreach(int i in Enumerable.Range(0,10000000))
-    r.Match(s);
 
-Console.WriteLine(string.Format("s.find: {0} sec", (DateTime.Now - start_time).TotalSeconds ));
+start_time = DateTime.Now;
+foreach (int i in Enumerable.Range(0, 100000000))
+    randomValue = rnd.NextDouble() * 100;
+Console.WriteLine(string.Format("Total: {0} sec", (DateTime.Now - start_time).TotalSeconds));
+
+start_time = DateTime.Now;
+foreach (int i in Enumerable.Range(0, 100000000))
+    randomValue = rnd.NextDouble() + rnd.Next();
+Console.WriteLine(string.Format("Total: {0} sec", (DateTime.Now - start_time).TotalSeconds));
+
---
+++ 27.EightHomework - task 53/Program.cs
@@ -0,0 +1,104 @@
+﻿// Print array ...
+void printArray(int[,] arrayToShow){
+
+    for (int i = 0; i < arrayToShow.GetLength(0); i++)
+    {
+        for (int j = 0; j < arrayToShow.GetLength(1); j++)
+            Console.Write($"{arrayToShow[i, j]} ");
+
+        Console.WriteLine();
+
+    }
+
+    Console.WriteLine();
+
+}
+
+// Sort array ...
+int[,] sortArray(int[,] arrayToSort){
+
+    int[,] sortedArrayData = new int[arrayToSort.GetLength(0), arrayToSort.GetLength(1)];
+    for (int i = 0; i < arrayToSort.GetLength(0); i++){
+
+        for (int collectID = 0; collectID < arrayToSort.GetLength(1); collectID++){
+
+            int maxID = 0;
+            for (int j = 0; j < arrayToSort.GetLength(1); j++)
+                if (arrayToSort[i, j] > arrayToSort[i, maxID])
+                    maxID = j;
+
+            sortedArrayData[i, collectID] = arrayToSort[i, maxID];
+            arrayToSort[i,maxID] = 0;
+
+        }
+
+    }
+
+    return sortedArrayData;
+
+}
+
+// Check that m or n is correct and return it if it's true ...
+int CheckArraySize(string incomingString)
+{
+
+    int attr = -1;
+
+    bool mOk = int.TryParse(incomingString, out attr);
+    if (!mOk)
+    {
+
+        Console.WriteLine("Value is not numeric ... ");
+        return -1;
+
+    }
+    else if (attr <= 0)
+    {
+
+        Console.WriteLine("Value is equal or less than zero ... ");
+        return -1;
+
+    }
+
+    return attr;
+
+}
+
+// Set variables ...
+int m = 0;
+int n = 0;
+
+// Get array size from user ...
+while (m <= 0 || n <= 0)
+{
+
+    if (m <= 0)
+    {
+        Console.WriteLine("Set rows count (m):");
+        m = CheckArraySize(Console.ReadLine());
+    }
+
+    if (n <= 0)
+    {
+        Console.WriteLine("Set columns count (n):");
+        n = CheckArraySize(Console.ReadLine());
+    }
+
+}
+
+int[,] array = new int[m, n];
+Random rnd = new Random();
+Console.Clear();
+
+for (int i = 0; i < array.GetLength(0); i++)
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = rnd.Next(1, 10);
+
+
+Console.WriteLine($"Base array [{m} x {n}]: ");
+
+printArray(array);
+array = sortArray(array);
+
+Console.WriteLine($"Sorted array: [{m} x {n}]");
+printArray(array);
