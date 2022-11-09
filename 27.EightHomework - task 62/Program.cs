1009555aefa90adc00cc102674587fc71298ffe0
Sergey M <berserker992@gmail.com>
2022-11-10 04:39:29 +1100

    eight homework v0.6

--- 27.EightHomework - task 62/Program.cs
+++ 27.EightHomework - task 62/Program.cs
@@ -10,7 +10,7 @@
 
 */
 
-// ...
+// Clear place after begin ...
 Console.Clear();
 
 // ...
@@ -20,7 +20,7 @@ void printArray(int[,] arrayToShow)
     for (int i = 0; i < arrayToShow.GetLength(0); i++)
     {
         for (int j = 0; j < arrayToShow.GetLength(1); j++)
-            Console.Write($"{arrayToShow[i, j]} ");
+            Console.Write(string.Format("{0:00}  ", arrayToShow[i, j]));
 
         Console.WriteLine();
 
@@ -31,6 +31,35 @@ void printArray(int[,] arrayToShow)
 }
 
 // Create array
+int sideSize = 4;
+int placeSize = sideSize * sideSize;
+int[,] A = new int[sideSize,sideSize];
 
+// Coordinates
+var C = new (int Row, int Coll)[] {
+
+                        (0,0), (0,1), (0,2), (0,3),
+                        (1,3), (2,3), (3,3), (3,2),
+                        (3,1), (3,0), (2,0), (1,0),
+                        (1,1), (1,2), (2,2), (2,1)
+
+                    };
+
+// Set variables
+int v = 1;
+int i = 0;
+int j = 0;
+
+// Fill array
+while(v <= placeSize){
+
+    i = C[v - 1].Row;
+    j = C[v - 1].Coll;
+
+    A[i,j] = v;
+    v += 1;
+
+}
 
 // Show array
+printArray(A);
