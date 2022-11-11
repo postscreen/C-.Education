402c9611f113ea507a0387de77189045c4587758
Sergey M <berserker992@gmail.com>
2022-11-12 00:42:32 +1100

    final homework v0.2

---
+++ 28.FinalHomework - task 64/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+
+Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+    N = 5 -> "5, 4, 3, 2, 1"
+    N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+*/
+
+// Clear place ...
+Console.Clear();
+
+// Set N ...
+int N = 10;
+
+// Recursion ...
+void showNaturalNumerics(int Max, int i = 0){
+
+    if(Max == 0) return;
+    else {
+
+        Console.WriteLine($"Iteration {i}: {Max}");
+        showNaturalNumerics(Max-1, i+1);
+
+    }
+
+}
+
+// Task solution ... 
+showNaturalNumerics(N);
---
+++ 28.FinalHomework - task 66/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+
+    M = 1; N = 15 -> 120
+    M = 4; N = 8. -> 30
+
+*/
+
+// Clear place ...
+Console.Clear();
+
+// Set N ...
+int M = 1;
+int N = 9;
+
+// Recursion ...
+int sumOfNaturalNumerics(int Min, int Max, int sum = 0)
+{
+
+    if (Max == 0) return sum;
+    else
+    {
+        
+        sum += Max;
+        Max--;
+        sum += sumOfNaturalNumerics(Min, Max - 1, sum);
+        return sum;
+
+    }
+
+    return sum;
+
+}
+
+// Task solution ... 
+int sum = sumOfNaturalNumerics(M, N);
+Console.WriteLine(sum);
