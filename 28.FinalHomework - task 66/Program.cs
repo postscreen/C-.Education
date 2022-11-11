07436bb80d7e104c75f1aebfd0a2ff5107bc8938
Sergey M <berserker992@gmail.com>
2022-11-12 08:08:49 +1100

    final homework v0.9

--- 28.FinalHomework - task 66/Program.cs
+++ 28.FinalHomework - task 66/Program.cs
@@ -15,9 +15,7 @@ int M = 1;
 int N = 15;
 
 // Recursion ...
-int sumOfNaturalNumerics(int Min, int Max) => 
-    Max == 0 ?  0 :
-    sumOfNaturalNumerics(Min, Max - 1) + Max;
+int sumOfNaturalNumerics(int Min, int Max) => Max == 0 ?  0 : sumOfNaturalNumerics(Min, Max - 1) + Max;
 
 // Task solution ... 
 Console.WriteLine(sumOfNaturalNumerics(M, N));
